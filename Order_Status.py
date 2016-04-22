b30130f992674f373d25b7a7bb7a6cc91b49a181
Armen Donigian <donigian@gmail.com>
2016-04-21 18:39:01 -0700

    Implemented State pattern for order status.

--- Order_Status.py
+++ Order_Status.py
@@ -1,3 +1,24 @@
 class Order_Status(object):
     status = None
 
+class Ready_To_Order(Order_Status):
+    def __init__(self):
+        print("Customer ready to order...")
+
+    def ready_to_order(self):
+        self.state = Order_Taken
+
+class Order_Taken(Order_Status):
+    def __init__(self):
+        print("Server took order...")
+
+class Preparing_Order(Order_Status):
+    def __init__(self):
+        print("Cook preparing order...")
+
+class Order_Prepared(Order_Status):
+    def __init__(self):
+        print("Cook prepared order...")
+
+order = Ready_To_Order()
+order.status = order.ready_to_order()
